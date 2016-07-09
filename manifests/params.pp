8a2f6485abf22165cd187ad99e3886d14cf17278
Roman Vasylenko <vrf888@users.noreply.github.com>
2016-07-08 19:46:02 -0700

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -115,7 +115,7 @@ class icinga2::params {
         #...but 14.04 does:
         '14.04': {
           $icinga2_server_package = 'icinga2'
-          $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-extra', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
+          $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
           $icinga2_server_mail_package = 'mailutils'
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $server_plugin_package_install_options = '--no-install-recommends'
